92aba08ebc2ee99000c6d82af8d1a3ef0dfd46e9
xiongqiang <xiongqiang@hebao.im>
2018-01-30 11:25:00 +0800

    determine PropTypes

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 /**
- * Created by hebao on 2018/1/23.
+ * Created by woowalker on 2018/1/23.
  */
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import PTRScrollList from './lib/PTRScrollList'
-module.exports = PTRScrollList
+
+export default class PTRControl extends Component {
+  static headerRefreshDone = () => PTRScrollList.headerRefreshDone()
+  static footerInfiniteDone = () => PTRScrollList.footerInfiniteDone()
+
+  static propTypes = {
+    scrollComponent: PropTypes.oneOf(['ScrollView', 'ListView', 'FlatList', 'VirtualizedList']).isRequired,
+
+    getRef: PropTypes.func,
+
+    enableHeaderRefresh: PropTypes.bool,
+    setHeaderHeight: PropTypes.number,
+    onTopReachedThreshold: PropTypes.number,
+    renderHeaderRefresh: PropTypes.func,
+    onHeaderRefreshing: PropTypes.func,
+
+    enableFooterInfinite: PropTypes.bool,
+    setFooterHeight: PropTypes.number,
+    onEndReachedThreshold: PropTypes.number,
+    renderFooterInfinite: PropTypes.func,
+    onFooterInfiniting: PropTypes.func,
+  }
+
+  render() {
+    return <PTRScrollList {...this.props}/>
+  }
+}
